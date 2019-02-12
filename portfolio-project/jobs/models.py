1a93b31d0acae35b173daefc9467ebfece614587
Adrian LaCour <adrianj.lacour@gmail.com>
2019-02-12 16:58:30 -0600

    First commit

---
+++ portfolio-project/jobs/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    jobName = models.CharField(max_length = 200, default='Project Details')
+    image = models.ImageField(upload_to = 'images/')
+    summary = models.CharField(max_length = 200, default='Summary is in GitHub ReadMe.')
+    githubURL = models.CharField(max_length = 200, default='https://github.com/Deep9z')
+    fullJobDetail = models.CharField(max_length = 2000, default='Summary is in GitHub ReadMe.')
+
+    def __str__(self):
+        return self.summary
