90548106d0f60fc6fa3333816e0c927f5b0aa800
Adrian LaCour <adrianj.lacour@gmail.com>
2019-02-14 17:59:51 +0000

    Deployed on pythonanywhere.com

--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     jobName = models.CharField(max_length = 200, default='Project Details')
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = settings.MEDIA_ROOT)
     summary = models.CharField(max_length = 200, default='Summary is in GitHub ReadMe.')
     githubURL = models.CharField(max_length = 200, default='https://github.com/Deep9z')
     fullJobDetail = models.CharField(max_length = 2000, default='Summary is in GitHub ReadMe.')
--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.mysite.com']
+ALLOWED_HOSTS = ['adrianlacour.pythonanywhere.com']
 
 
 # Application definition
@@ -77,12 +76,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'AquaF1na',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/adrianlacour/portfolio-website/portfolio-project/static'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = '/home/adrianlacour/portfolio-website/portfolio-project/static/'
 
-#django_heroku.settings(locals())
